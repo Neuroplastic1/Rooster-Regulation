891b2ce535b9e68d0bcf2461efb354f9ac3174b1
Neuroplastic1 <shafqat.umair@gmail.com>
2021-10-06 01:02:59 -0400

    Tests added for timeAtDawn() features

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 // Define a rooster
 Rooster = {};
 
+// the first test in index_test.js is testing that the Rooster.announceDawn() is returning the expected output 'cock-a-doodle-doo!'
 // Return a morning rooster call
 Rooster.announceDawn = () => {
   return 'cock-a-doodle-doo!';
 }
-
-// Return hour as string
-// Throws Error if hour is not between 0 and 23 inclusive
+// in the second it block in index_test.js will test if timeAtDawn() returns a string
+// the next it block checks if it Throws an Error if hour is not between 0 and 23 inclusive
+// notice the rangeError is a form of error that means when we test (if rangeError works), we will expect rangeError as an output from this function when we throw this func inside assert.throws().
+// asser.throws() takes two arguments. The first argument will be a function which calls .timeAtDawn() and the second argument will be the error you expect to be thrown
+// then when we'll run the test script with invalid input, we'll get to see if RangeError works as expected
 Rooster.timeAtDawn = (hour) => {
   if (hour < 0 || hour > 23) {
     throw new RangeError;
--- test/index_test.js
+++ test/index_test.js
@@ -3,16 +3,68 @@ const assert = require('assert');
 // import Rooster from index.js
 const Rooster = require('../index');
 
+// testing Rooster.announceDawn()'s output
+// A describe block for Rooster
 describe('Rooster', () => {
-   describe('.announceDawn', () => {
+  // Within the first block write another describe block for the .announceDawn method
+  describe('.announceDawn', () => {
+    // Within the nested describe block, write an it block that tests that the method 'returns a rooster call'
+    // four phases of a test: setup, exercise, and verify in this it block (teardown optional for now)
     it('returns a rooster call', () => {
-       // Defining expected output
-       const expected = 'cock-a-doodle-doo!';
+      // Setup -  Defining expected output
+      const expected = 'cock-a-doodle-doo!';
 
-      // Calling Rooster.announceDawn and store result in variable
-       const contents = Rooster.announceDawn();
-      // Useing an assert method to compare actual and expected result
-      assert.strictEqual(contents,expected);
+      // exercise - call the function under test - Calling Rooster.announceDawn and store result in variable
+      const actual = Rooster.announceDawn();
+      // verify - Using an assert method to compare actual and expected output
+      assert.strictEqual(actual, expected);
     });
- });
+  });
+
+
+  //Test that .timeAtDawn 'returns its argument as a string'
+  describe('.timeAtDawn', () => {
+    it('returns its argument as a string', () => {
+      // this time we can use an input for testing our function Rooster.timeAtDawn(hour)
+      const inputHour = 12;
+      //setup
+      const expected = '12';
+      // excercise
+      const actual = Rooster.timeAtDawn(12);
+      // verify
+      assert.strictEqual(actual, expected);
+    });
+
+    // this it block tests if .timeAtDawn(hour) throws an error if the valid (hour) parameter(hour < 0 || hour > 23) is not provided
+    // first checking if it throws an error giving it an inputHour less than 0
+    it('throws an error if passed a number less than 0', () => {
+      //setup
+      const inputHour = -1;
+      const expected = RangeError;
+      // verify
+      assert.throws(
+        () => {
+          // excercise
+          Rooster.timeAtDawn(inputHour);
+        },
+        expected
+      );
+    });
+
+    // test hour's upper limit
+    it('throws an error if passed a number more than 23', () => {
+      //setup
+      const inputHour = 24;
+      const expected = RangeError;
+      // verify
+      assert.throws(
+        () => {
+          // excercise
+          Rooster.timeAtDawn(inputHour);
+        },
+        expected
+      );
+    });
+
+  });
 });
